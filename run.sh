bed10e7bfa1bb805f878f025cd9cb50d49b34c19
Pierson Brannan <plbrannan95@gmail.com>
2020-12-21 14:28:40 -0500

    Add lookup by username

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-curl https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=2bananamanas
+#!/bin/bash
+
+# Retrieve username
+username=$(<name.txt)
+
+# Retrieve stats
+data=$(curl https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=$username)
+
+python3 src/main.py $data
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys
 import matplotlib.pyplot as plt
 
+#  SECTION - Retrieve stats
 # def hello(variable):
 #     print(variable)
 
@@ -8,15 +9,24 @@ import matplotlib.pyplot as plt
 
 # hello(data)
 
+
+
+# SECTION - Parse stats
+
+
+
+# SECTION - Create graph
+
 # Data to plot
-labels = 'Python', 'C++', 'Ruby', 'Java'
+labels = 'Attack', 'Defense', 'Strength', 'Hitpoints'
+# 'Ranged', 'Prayer', 'Magic', 'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking', 'Crafting', 'Smithing', 'Mining', 'Herblore', 'Agility', 'Thieving', 'Slayer', 'Farming', 'Runecraft', 'Hunter', 'Construction'
 sizes = [215, 130, 245, 210]
 colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
 explode = (0.1, 0, 0, 0)  # explode 1st slice
 
 # Plot
 plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-autopct='%1.1f%%', shadow=True, startangle=140)
+        autopct='%1.1f%%', shadow=True, startangle=140)
 
 plt.axis('equal')
 plt.show()
