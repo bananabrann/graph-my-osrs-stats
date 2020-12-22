4f6d5499d22444d43e70efd11a963d9dc1f1b9ac
Pierson Brannan <plbrannan95@gmail.com>
2020-12-21 21:11:47 -0500

    Full MVP

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from LevelStat import LevelStat
 
 
-
 # SECTION - Parse stats
 data = sys.argv[1].split("\n")
 rank = data[0].split(',')[0]            # TODO - Make this more effecient
@@ -36,17 +35,23 @@ ls_con = LevelStat("construction", data[23])
 
 
 
-# SECTION - Create graph
+# SECTION - Graph creation
+# NOTE - For more information, visit https://pythonspot.com/matplotlib-pie-chart/
+# TODO - Can this be more effecient? What if everything was stored as a dict, and then just passing
+#    that dict on directly to the pie object?
+labels = ['Attack', 'Defense', 'Strength', 'Hitpoints', 'Ranged', 'Prayer', 'Magic', 'Cooking',
+          'Woodcutting', 'Fletching', 'Fishing', 'Firemaking', 'Crafting', 'Smithing', 'Mining',
+          'Herblore', 'Agility', 'Thieving', 'Slayer', 'Farming', 'Runecraft', 'Hunter',
+          'Construction']
+sizes = [ls_atk.xp, ls_def.xp, ls_str.xp, ls_hit.xp, ls_ran.xp, ls_pry.xp, ls_mag.xp, ls_cok.xp,
+         ls_wcg.xp, ls_fle.xp, ls_fsh.xp, ls_fmk.xp,
+         ls_cra.xp, ls_smi.xp, ls_min.xp, ls_hrb.xp, ls_agl.xp, ls_thv.xp, ls_sly.xp, ls_far.xp,
+         ls_rnc.xp, ls_hun.xp, ls_con.xp]
+colors = ['#b50c00', '#65c7c5', '#387328', '#e2e6e1', '#868f47', '#f5f7e6', '#2759b0', '#701596',
+          '#91712c', '#1d5454', '#adcdd9', '#fa9923', '#613418', '#706158', '#87827f', '#4e8042',
+          '#031b40', '#4e2b5e', '#0d0d0d', '#0e7810', '#bdaf1a', '#5c5a54', '#b8a46a']
 
-# # Data to plot
-# labels = 'Attack', 'Defense', 'Strength', 'Hitpoints', 'Ranged', 'Prayer', 'Magic', 'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking', 'Crafting', 'Smithing', 'Mining', 'Herblore', 'Agility', 'Thieving', 'Slayer', 'Farming', 'Runecraft', 'Hunter', 'Construction'
-# sizes = [215, 130, 245, 210]
-# colors = ['#333333', 'yellowgreen', 'lightcoral', 'lightskyblue']
-# explode = (0.1, 0, 0, 0)  # explode 1st slice
-
-# # Plot
-# plt.pie(sizes, explode=explode, labels=labels, colors=colors,
-#         autopct='%1.1f%%', shadow=True, startangle=140)
-
-# plt.axis('equal')
-# plt.show()
+plt.pie(sizes, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
